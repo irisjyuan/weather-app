1f06c7c251fc60c660451d71c23ac02021aa6a26
Iris Yuan <iris.yuan@loom.com>
2022-04-18 14:00:02 -0700

    no more redux there's nothing to track for state so it's just api practice now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
-import { Location } from './features/weather/Location';
+import { Location } from './Location';
 import './App.css';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <h2>Weather App</h2>
       <Location />
-      <Counter />
     </div>
   );
 }
--- src/Location.js
+++ src/Location.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { updateLocation } from './locationSlice';
 
 export function Location() {
   const [location, setLocation] = useState('');
